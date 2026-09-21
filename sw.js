a0641d1763e1e20f9f1eddab3f05b9a4dd0a186c
Brian Ngugi <blink.bn9@gmail.com>
2026-09-21 16:55:52 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-const CACHE_NAME = 'sms-font-v50'; 
+const CACHE_NAME = 'sms-system-v51';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './true_logo.png',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
-  'https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2'
+  './true_logo.png'
 ];
 
 self.addEventListener('install', e => {
@@ -30,18 +28,15 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-// NETWORK FIRST: Always check GitHub for updates before using offline files
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).then(networkResponse => {
-      // If found on network, save a copy to cache and return it
       if (networkResponse && networkResponse.status === 200) {
         const copy = networkResponse.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
       }
       return networkResponse;
     }).catch(() => {
-      // If offline, use the saved cache
       return caches.match(e.request).then(response => {
         if (response) return response;
         if (e.request.mode === 'navigate') return caches.match('./index.html');
