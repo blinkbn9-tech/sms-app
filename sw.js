b0649a2c64597e471ab457914c2a4c382e16c0a4
Brian Ngugi <blink.bn9@gmail.com>
2026-09-21 16:37:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'sms-font-v48'; 
+const CACHE_NAME = 'sms-font-v49'; 
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './true_logo.png', 
+  './true_logo.png',
   'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
-  'https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2'
+  'https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2' // The actual font file
 ];
 
 self.addEventListener('install', e => {
